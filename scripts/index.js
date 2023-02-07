924148432ac3e5c5283ffd8e19f0f5ebab41884f
Ragna-A4 <ragnare4ek@yandex.ru>
2023-02-08 01:01:51 +0300

    fix: form

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,30 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close-button');
 let formElement = popup.querySelector('.popup__form');
-let nameInput = popup.querySelector('.popup__name');
-let jobInput = popup.querySelector('.popup__job');
+let nameInput = popup.querySelector('.popup__item_el_name');
+let jobInput = popup.querySelector('.popup__item_el_job');
 let nameProfile = document.querySelector('.profile__user-name');
 let jobProfile = document.querySelector('.profile__user-job');
 
 let handleEditButtonClick = () => {
     popup.classList.add('popup_opened');
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
+}
+
+let closePopup =() => {
+    popup.classList.remove('popup_opened');
 }
 
 let handleCloseButtonClick = () => {
-    popup.classList.remove('popup_opened');
+    closePopup();
 }
 
 let handleFormSubmit = (evt) => {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    closePopup();
 }
     
 editButton.addEventListener('click', handleEditButtonClick);
