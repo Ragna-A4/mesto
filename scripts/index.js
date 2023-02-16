928d6f5c0bf612c29278f70eb1cfdedbedbde9fe
Ragna-A4 <ragnare4ek@yandex.ru>
2023-02-16 16:05:14 +0300

    feat: fill the gallery (js), activate like button

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,114 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = popup.querySelector('.popup__close-button');
-let formElement = popup.querySelector('.popup__form');
-let nameInput = popup.querySelector('.popup__item_el_name');
-let jobInput = popup.querySelector('.popup__item_el_job');
-let nameProfile = document.querySelector('.profile__user-name');
-let jobProfile = document.querySelector('.profile__user-job');
+const initialCards = [
+    {
+      name: 'Пляж',
+      link: './images/beach.jpg'
+    },
+    {
+      name: 'Кораллы',
+      link: './images/corals.jpg'
+    },
+    {
+      name: 'Остров',
+      link: './images/island.jpg'
+    },
+    {
+      name: 'Медуза',
+      link: './images/jellyfish.jpg'
+    },
+    {
+      name: 'Закат',
+      link: './images/sunset.jpg'
+    },
+    {
+      name: 'Кашалот',
+      link: './images/fin_whale.jpg'
+    }
+  ]; 
 
-let handleEditButtonClick = () => {
+const gallery = document.querySelector('.gallery');
+const template = document.getElementById('card');
+
+
+const getCardElement = (element) => {
+    const newCardElement = template.content.cloneNode(true);
+    const newCardImage = newCardElement.querySelector('.card__image');
+    newCardImage.src = element.link;
+    newCardImage.alt = element.name;
+    const newCardNaming = newCardElement.querySelector('.card__naming');
+    newCardNaming.textContent = element.name;
+    const cardButtonTrash = newCardElement.querySelector('.card-button__trash');
+    const cardButtonLike = newCardElement.querySelector('.card-button__like');
+    cardButtonTrash.addEventListener('click', function(evt) {
+        evt.target.closest('card').remove();
+    });
+    cardButtonLike.addEventListener('click', function(evt) {
+        evt.target.classList.toggle('card-button__like_active')
+    });
+    const newImagePopupPicture = newCardElement.querySelector('.image-popup__picture');
+    newImagePopupPicture.src = element.link;
+    newImagePopupPicture.alt = element.name;
+    const newImagePopupNaming = newCardElement.querySelector('.image-popup__naming');
+    newImagePopupNaming.textContent = element.name;
+    return newCardElement;
+}
+
+const renderCard = (wrap, element) => {
+    wrap.append(getCardElement(element))
+}
+
+initialCards.forEach((element) => {
+    renderCard(gallery, element);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const cardImage = document.querySelector('.card__image');
+const imagePopup = document.querySelector('.image-popup');
+
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = popup.querySelector('.popup__close-button');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = popup.querySelector('.popup__item_el_name');
+const jobInput = popup.querySelector('.popup__item_el_job');
+const nameProfile = document.querySelector('.profile__user-name');
+const jobProfile = document.querySelector('.profile__user-job');
+
+const handleCardImageClick = () => {
+    imagePopup.classList.add('image-popup_opened');
+}
+
+const handleEditButtonClick = () => {
     popup.classList.add('popup_opened');
     nameInput.value = nameProfile.textContent;
     jobInput.value = jobProfile.textContent;
 }
 
-let closePopup =() => {
+const closePopup = () => {
     popup.classList.remove('popup_opened');
 }
 
-let handleCloseButtonClick = () => {
-    closePopup();
+const closeImagePopup = () => {
+    imagePopup.classList.remove('image-popup_opened');
 }
 
-let handleFormSubmit = (evt) => {
+const handleCloseButtonClick = () => { 
+    closeImagePopup();
+    closePopup();
+}
+const handleFormSubmit = (evt) => {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
@@ -31,3 +118,4 @@ let handleFormSubmit = (evt) => {
 editButton.addEventListener('click', handleEditButtonClick);
 closeButton.addEventListener('click', handleCloseButtonClick);
 formElement.addEventListener('submit', handleFormSubmit);
+cardImage.addEventListener('click', handleCardImageClick);
