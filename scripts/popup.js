5c924b4b6cf6bdefb0009eafa5ab6015ef9dffd6
Ragna-A4 <ragnare4ek@yandex.ru>
2023-04-15 19:34:22 +0300

    refactor: add Popup class

---
+++ scripts/popup.js
@@ -0,0 +1,35 @@
+export default class Popup {
+    constructor(popupSelector) {
+        this._popup = document.querySelector(popupSelector);
+    }
+
+    //закрытие попапа при нажатии на "esc"
+    _handleEscClose = (e) => {
+        if (e.key === "Escape") {
+            this.closePopup();
+        };
+    }
+
+    //открытие попапа
+    openPopup() {
+        this._popup.classList.add('popup_opened');
+        document.addEventListener('keydown', this._handleEscClose);
+    }
+
+    //закрытие попапа
+    closePopup() {
+        this._popup.classList.remove('popup_opened');
+        document.removeEventListener('keydown', this._handleEscClose);
+    }
+
+    setEventListeners() {
+        this._popup.addEventListener('mousedown', (evt) => {
+            if(evt.target.classList.contains('popup_opened')) {
+                this.closePopup();
+            }
+            if(evt.target.classList.contains('close-button')) {
+                this.closePopup();
+            }
+        });
+    }
+}
